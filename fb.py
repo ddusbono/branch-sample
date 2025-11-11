3d6b7ece7ebfdf793e57be93d3a43052a305c51a
ddusbono <ddusbono@gmail.com>
2025-11-11 14:16:13 +0900

    refatctor: Do fizzbuzz 1 if..

--- fb.py
+++ fb.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 for j in range(4, 15+1):
-    if j % 15 == 0:
-        print('fizzbuzz')
-    elif j % 5 == 0:
-        print('buzz')
-    elif j % 3 == 0:
-        print('fizz')
+    if j % 3 == 0 or j % 5 == 0:
+        print('fizz' * (j%3==0) + 'buzz' * (j%5==0))
     else:
         print(j)
