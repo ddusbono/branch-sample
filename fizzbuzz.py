684f60d3dcf5d4ef1cff5a0a99233372d6d3b176
ddusbono <ddusbono@gmail.com>
2025-11-11 15:40:50 +0900

    feat : Do fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,9 @@
 for i in range(1, 15+1):
-    if i % 3 == 0:
-        print('fizz' * (i%3==for i in range(1, 15 + 1):
-    # i를 3으로 나눈 나머지가 0인지 (3의 배수인지) 확인
-    if i % 3 == 0:
+    if i % 5 == 0:
+        print('buzz' * (i%3==for i in range(1, 15 + 1):
+    if i % 5 == 0:
         print('buzz')
     else:
-        # 3의 배수가 아니면 숫자 자체를 출력
         print(i)0) + 'buzz' * (i%5==0))
     else:
         print(i)
