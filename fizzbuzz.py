fa05f907a60f52bd25052b5d726eded3189df2f9
ddusbono <ddusbono@gmail.com>
2025-11-11 15:57:37 +0900

    feat : Create fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,1 @@
-for i in range(1, 15+1):
-    if i % 5 == 0:
-        print('buzz' * (i%3==for i in range(1, 15 + 1):
-    if i % 5 == 0:
-        print('buzz')
-    else:
-        print(i)0) + 'buzz' * (i%5==0))
-    else:
-        print(i)
+print('hello')
